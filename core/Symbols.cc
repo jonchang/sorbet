1075896a28cdf722f96672220a881fe5f788eb74
Kaan Ozkan <kaan.ozkan@shopify.com>
2020-09-15 18:03:26 -0400

    rename generate module

--- core/Symbols.cc
+++ core/Symbols.cc
@@ -814,7 +814,7 @@ string Symbol::toStringWithOptions(const GlobalState &gs, int tabs, bool showFul
         }
 
         if (pair.first == Names::singleton() || pair.first == Names::attached() ||
-            pair.first == Names::classMethods()) {
+            pair.first == Names::mixedInClassMethods()) {
             continue;
         }
 
